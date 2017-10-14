0d15ddb76aeb3c3b766b812239e9d7eacd1da7ee
Jasmine Chao <ejasminechao@gmail.com>
2017-10-14 17:19:37 +0200

    finished creating features overview

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ features_attributes = [
     subhead: "Tracking Emails Made Easy",
     description: "If you are wondering what happens to your outgoing emails after you hit send, then you’ve come to the right place. Meet Mailbutler - the email extension, which allows you to track emails in Apple Mail and Gmail.",
     video_link: "https://www.youtube.com/ watch?v=BriJb4YNyqo",
-    cloundinary_img_url: "https://www.mailbutler.io/images/icon_512x512@2x.png"
+    cloundinary_img_id: "bhwkf7wvspkox0k22rvl"
   },
 ]
 
