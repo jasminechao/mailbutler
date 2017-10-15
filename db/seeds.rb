665ab39a5269c1bd07dd4e7f7cbbaa7fcb20d84e
Jasmine Chao <ejasminechao@gmail.com>
2017-10-15 02:20:20 +0200

    added links cards continer and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,27 @@ features_attributes = [
     subhead: "Tracking Emails Made Easy",
     description: "If you are wondering what happens to your outgoing emails after you hit send, then you’ve come to the right place. Meet Mailbutler - the email extension, which allows you to track emails in Apple Mail and Gmail.",
     video_link: "https://www.youtube.com/ watch?v=BriJb4YNyqo",
-    cloundinary_img_id: "bhwkf7wvspkox0k22rvl"
+    cloundinary_img_id: "Icon_Tracking"
   },
   {
-    header: "Email Tracking",
-    subhead: "Tracking Emails Made Easy",
-    description: "If you are wondering what happens to your outgoing emails after you hit send, then you’ve come to the right place. Meet Mailbutler - the email extension, which allows you to track emails in Apple Mail and Gmail.",
-    video_link: "https://www.youtube.com/ watch?v=BriJb4YNyqo",
-    cloundinary_img_id: "bhwkf7wvspkox0k22rvl"
+    header: "Snooze",
+    cloundinary_img_id: "Icon_Snooze"
+  },
+  {
+    header: "Signature",
+    cloundinary_img_id: "Icon_Signature"
+  },
+  {
+    header: "Task",
+    cloundinary_img_id: "Icon_Task"
+  },
+  {
+    header: "Send Later",
+    cloundinary_img_id: "Icon_SendLater"
+  },
+  {
+    header: "Attachment",
+    cloundinary_img_id: "Icon_Attachment"
   },
 ]
 
